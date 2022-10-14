c69ea432dc78a9489ca62d00cf80840d3e0653cc
meet1201 <meet12.work@gmail.com>
2022-10-14 16:03:12 -0700

    First Commit

---
+++ pc assistant.py
@@ -0,0 +1,34 @@
+import speech_recognition as sr
+from gtts import gTTS
+import playsound
+import os
+
+r=sr.Recognizer()
+def voice_command_processor():
+    with sr.Microphone() as source:
+        audio=r.listen(source,phrase_time_limit=4)
+        text=''
+        try:
+            text=r.recognize_google(audio)
+        except sr.UnknownValueError as e:
+            print(e)
+        except sr.RequestError as e:
+            print("Service is down")
+
+            return text.lower()
+
+def audio_playback(text):
+    filename="test.mp3"
+    tts=gTTS(text=text, lang='en-us')
+    tts.save(filename)
+    playsound.playsound(filename)
+    os.remove(filename)
+
+def execute_voice_command(text):
+    if "what are you " in text:
+        audio_playback("I am a voice assistant")
+
+while True:
+    command=voice_command_processor()
+    print(command)
+    execute_voice_command(command
